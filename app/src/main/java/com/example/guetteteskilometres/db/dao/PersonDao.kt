5fe7cb073083de4b6aa4ae26aa313d5d54a6b967
fcharrette <faustine.charrette@scalian.com>
2024-06-19 14:54:10 +0200

    Ordre alphabétique

--- app/src/main/java/com/example/guetteteskilometres/db/dao/PersonDao.kt
+++ app/src/main/java/com/example/guetteteskilometres/db/dao/PersonDao.kt
@@ -11,7 +11,7 @@ interface PersonDao {
     @Insert
     suspend fun insertPerson(personEntity: PersonEntity): Long?
 
-    @Query("select * from person where id_event = :id")
+    @Query("select * from person where id_event = :id order by firstname")
     fun getEventPersons(id: Long): Flow<List<PersonEntity>>
 
     @Query("select * from person where id = :idPerson")
